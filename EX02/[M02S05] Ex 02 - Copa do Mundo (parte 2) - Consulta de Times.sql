c0a24c8b495f08ac08019fd043f128ba456ca392
Marcelo Junqueira Teixeira <marcelo.teixeira@softplan.com.br>
2023-10-19 13:11:15 -0300

    [M02S05] Ex 02 - Copa do Mundo (parte 2) - Consulta de Times

---
+++ EX02/[M02S05] Ex 02 - Copa do Mundo (parte 2) - Consulta de Times.sql
@@ -0,0 +1,5 @@
+--a) Consultar o nome da seleção cuja sigla é 'KSA'.
+SELECT nome FROM times WHERE sigla='KSA';
+
+--b) Consultar a sigla da seleção de Gana.
+SELECT sigla FROM times WHERE nome='Gana';
